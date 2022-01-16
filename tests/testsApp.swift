5885e3fd6969ad413fe5401d1903d9d11a4f2b1f
deffuy <taku.bellf@gmail.com>
2022-01-16 20:40:58 +0900

    Initial Commit

---
+++ tests/testsApp.swift
@@ -0,0 +1,17 @@
+//
+//  testsApp.swift
+//  tests
+//
+//  Created by 山川拓朗 on 2022/01/16.
+//
+
+import SwiftUI
+
+@main
+struct testsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
